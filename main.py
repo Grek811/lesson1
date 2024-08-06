43ea34db1ab76fa971e9f9d123c42458e9031781
Grek811 <164835703+Grek811@users.noreply.github.com>
2024-08-06 06:42:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# 1st program
+print((9*5*5)**0.5)
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+# 3rd program
+a = 2*2+2
+b = 2*(2+2)
+print(a == b)
+# 4th program
+x = 123.456
+print(int(x * 10) % 10)
